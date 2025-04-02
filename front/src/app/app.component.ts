8663a6c2309e205574ffd4645b36cc7900290664
Saya Mirkulova <zansaaumirkulova@gmail.com>
2025-04-02 19:08:08 +0500

    updated2

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Icon1Component } from './icon1/icon1.component';
+import { Icon2Component } from './icon2/icon2.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Icon1Component, Icon2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ front/src/app/icon2/icon2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-icon2',
+  imports: [RouterOutlet],
+  templateUrl: './icon2.component.html',
+  styleUrl: './icon2.component.scss'
+})
+export class Icon2Component {
+
+}
